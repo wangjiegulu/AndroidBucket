d6fd4a73571c60292da0773cfd7b8a17f44c3d6d
王杰 <tiantian.china.2@gmail.com>
2014-09-12 11:24:55 +0800

    增加Shape工具

---
+++ src/com/wangjie/androidbucket/utils/imageprocess/ABShape.java
@@ -0,0 +1,34 @@
+package com.wangjie.androidbucket.utils.imageprocess;
+
+import android.graphics.Paint;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RoundRectShape;
+import android.graphics.drawable.shapes.Shape;
+
+/**
+ * Author: wangjie
+ * Email: tiantian.china.2@gmail.com
+ * Date: 9/12/14.
+ */
+public class ABShape {
+
+    /**
+     * 生成圆角的背景
+     * @param color
+     * @return
+     */
+    public static ShapeDrawable generateCornerShapeDrawable(int color, int corner){
+        return generateCornerShapeDrawable(color, corner, corner, corner, corner);
+    }
+    public static ShapeDrawable generateCornerShapeDrawable(int color, int topLeftCorner, int topRightCorner, int bottomRightCorner, int bottomLeftCorner){
+        Shape shape = new RoundRectShape(new float[]{topLeftCorner, topLeftCorner, topRightCorner, topRightCorner, bottomRightCorner, bottomRightCorner, bottomLeftCorner, bottomLeftCorner}, null, null);
+        ShapeDrawable sd = new ShapeDrawable(shape);
+        sd.getPaint().setColor(color);
+        sd.getPaint().setStyle(Paint.Style.FILL);
+        return sd;
+    }
+
+
+
+
+}
