dfbd0cdaddd65a436c60dd0646767f408c516ae0
Hubert He <imfoolishman@gmail.com>
2014-12-29 15:13:49 +0800

    Formate code

--- src/com/wangjie/androidbucket/utils/ABFileUtil.java
+++ src/com/wangjie/androidbucket/utils/ABFileUtil.java
@@ -292,26 +292,6 @@ public class ABFileUtil {
      * @return
      */
     public static File uri2File(Context context, Uri uri) {
-<<<<<<< HEAD
-        // 而managedquery在api 11 被移除，所以要转为使用CursorLoader,并使用loadInBackground来返回
-        String[] projection = {MediaStore.Images.Media.DATA};
-        CursorLoader loader = new CursorLoader(context, uri, projection, null, null, null);
-        Cursor cursor = loader.loadInBackground();
-        if (cursor != null) {
-            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
-            return new File(cursor.getString(column_index));
-        }
-        return null;
-=======
-
-        // 在api level 11前可以用以下代码
-//        String[] proj = { MediaStore.Images.Media.DATA };
-//        Cursor actualimagecursor = context.managedQuery(uri,proj,null,null,null);
-//        int actual_image_column_index = actualimagecursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-//        actualimagecursor.moveToFirst();
-//        String img_path = actualimagecursor.getString(actual_image_column_index);
-//        File file = new File(img_path);
 
         // 而managedquery在api 11 被弃用，所以要转为使用CursorLoader,并使用loadInBackground来返回
         try {
@@ -329,13 +309,13 @@ public class ABFileUtil {
         }
         return null;
     }
-    public static File uri2FileInteral(Context context, Uri uri){
-        if(null == uri){
+
+    public static File uri2FileInteral(Context context, Uri uri) {
+        if (null == uri) {
             return null;
         }
         File file = uri2File(context, uri);
         return null == file ? new File(uri.getPath()) : file;
->>>>>>> 68a4bc7cbe1e56cbbf14f631252ed3a48ec09781
     }
 
 
@@ -478,11 +458,12 @@ public class ABFileUtil {
 
     /**
      * 扫描目录（扫描后可以及时在图库中看到）
+     *
      * @param context
      * @param path
      */
     @TargetApi(Build.VERSION_CODES.FROYO)
-    public static void scanFile(Context context, String path){
+    public static void scanFile(Context context, String path) {
         MediaScannerConnection.scanFile(context, new String[]{path}, null, null);
     }
 
