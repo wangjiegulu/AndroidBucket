425bbb1f3e72202e54ed99e47c6340f23965a247
zoeice525 <zoeice525@126.com>
2015-01-12 18:15:28 +0800

    获取手机网络信息

---
+++ src/com/wangjie/androidbucket/utils/Constants.java
@@ -0,0 +1,32 @@
+package com.wangjie.androidbucket.utils;
+
+/**
+ * Created by zoe on 15/1/6.
+ */
+public class Constants {
+    /**
+     * Unknown network class
+     */
+    public static final int NETWORK_CLASS_UNKNOWN = 0;
+
+    /**
+     * wifi net work
+     */
+    public static final int NETWORK_WIFI = 1;
+
+    /**
+     * "2G" networks
+     */
+    public static final int NETWORK_CLASS_2_G = 2;
+
+    /**
+     * "3G" networks
+     */
+    public static final int NETWORK_CLASS_3_G = 3;
+
+    /**
+     * "4G" networks
+     */
+    public static final int NETWORK_CLASS_4_G = 4;
+
+}
