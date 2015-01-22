f2c3567262e9e94820536ed5d2e8ef69d3563dc4
王杰 <tiantian.china.2@gmail.com>
2015-01-22 16:20:13 +0800

    解决修改删除医生工作经历和教育经历后没有及时刷新ui的bug

---
+++ src/com/wangjie/androidbucket/support/recyclerview/adapter/ABRecyclerViewHeaderFooterAdapter.java
@@ -0,0 +1,21 @@
+package com.wangjie.androidbucket.support.recyclerview.adapter;
+
+import com.wangjie.androidbucket.adapter.typeadapter.ABAdapterTypeRender;
+
+/**
+ * Author: wangjie
+ * Email: tiantian.china.2@gmail.com
+ * Date: 1/22/15.
+ */
+public class ABRecyclerViewHeaderFooterAdapter<T> extends ABRecyclerViewTypeAdapter{
+
+    @Override
+    public ABAdapterTypeRender<ABRecyclerViewHolder> getAdapterTypeRender(int type) {
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
