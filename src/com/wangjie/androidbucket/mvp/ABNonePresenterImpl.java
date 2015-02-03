7a82d7a3e6208910b2caea58076be78ec2cfd4b5
王杰 <tiantian.china.2@gmail.com>
2015-02-03 17:42:04 +0800

    新增ABNonePresenterImpl，AIPresenter中默认根据当前注解的数据field来生成实例

---
+++ src/com/wangjie/androidbucket/mvp/ABNonePresenterImpl.java
@@ -0,0 +1,9 @@
+package com.wangjie.androidbucket.mvp;
+
+/**
+ * Author: wangjie
+ * Email: tiantian.china.2@gmail.com
+ * Date: 2/3/15.
+ */
+public class ABNonePresenterImpl extends ABBasePresenter{
+}
