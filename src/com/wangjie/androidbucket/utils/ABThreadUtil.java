b7f7812d60e8ba3202a657d3d12c042e83de4e07
Hubert He <imfoolishman@gmail.com>
2015-04-10 23:07:51 +0800

    add

---
+++ src/com/wangjie/androidbucket/utils/ABThreadUtil.java
@@ -0,0 +1,22 @@
+package com.wangjie.androidbucket.utils;
+
+import com.wangjie.androidbucket.log.Logger;
+
+/**
+ * Author: wangjie
+ * Email: tiantian.china.2@gmail.com
+ * Date: 7/21/14.
+ */
+public class ABThreadUtil {
+    public static final String TAG = ABThreadUtil.class.getSimpleName();
+
+    public static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            Logger.e(TAG, e);
+        }
+    }
+
+
+}
---
+++ src/com/wangjie/androidbucket/utils/ABViewUtil.java
@@ -0,0 +1,138 @@
+package com.wangjie.androidbucket.utils;
+
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: wangjie  email:tiantian.china.2@gmail.com
+ * Date: 14-4-3
+ * Time: 下午1:47
+ */
+public class ABViewUtil {
+
+    /**
+     * 适用于Adapter中简化ViewHolder相关代码
+     *
+     * @param convertView
+     * @param id
+     * @param <T>
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public static <T extends View> T obtainView(View convertView, int id) {
+        SparseArray<View> holder = (SparseArray<View>) convertView.getTag();
+        if (holder == null) {
+            holder = new SparseArray<View>();
+            convertView.setTag(holder);
+        }
+        View childView = holder.get(id);
+        if (childView == null) {
+            childView = convertView.findViewById(id);
+            holder.put(id, childView);
+        }
+        return (T) childView;
+    }
+
+
+    /**
+     * view设置background drawable
+     *
+     * @param view
+     * @param drawable
+     */
+    public static void setBackgroundDrawable(View view, Drawable drawable) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+            view.setBackgroundDrawable(drawable);
+        } else {
+            view.setBackground(drawable);
+        }
+    }
+
+
+    /**
+     * 获取控件的高度，如果获取的高度为0，则重新计算尺寸后再返回高度
+     *
+     * @param view
+     * @return
+     */
+    public static int getViewMeasuredHeight(View view) {
+//        int height = view.getMeasuredHeight();
+//        if(0 < height){
+//            return height;
+//        }
+        calcViewMeasure(view);
+        return view.getMeasuredHeight();
+    }
+
+    /**
+     * 获取控件的宽度，如果获取的宽度为0，则重新计算尺寸后再返回宽度
+     *
+     * @param view
+     * @return
+     */
+    public static int getViewMeasuredWidth(View view) {
+//        int width = view.getMeasuredWidth();
+//        if(0 < width){
+//            return width;
+//        }
+        calcViewMeasure(view);
+        return view.getMeasuredWidth();
+    }
+
+    /**
+     * 测量控件的尺寸
+     *
+     * @param view
+     */
+    public static void calcViewMeasure(View view) {
+//        int width = View.MeasureSpec.makeMeasureSpec(0,View.MeasureSpec.UNSPECIFIED);
+//        int height = View.MeasureSpec.makeMeasureSpec(0,View.MeasureSpec.UNSPECIFIED);
+//        view.measure(width,height);
+
+        int width = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        int expandSpec = View.MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, View.MeasureSpec.AT_MOST);
+        view.measure(width, expandSpec);
+    }
+
+    public static int getAllListViewSectionCounts(ListView lv, List dataSource) {
+        if (null == lv || ABTextUtil.isEmpty(dataSource)) {
+            return 0;
+        }
+        return dataSource.size() + lv.getHeaderViewsCount() + lv.getFooterViewsCount();
+    }
+
+    /**
+     * 使用ColorFilter来改变亮度
+     *
+     * @param imageview
+     * @param brightness
+     */
+    public static void changeBrightness(ImageView imageview, float brightness) {
+        imageview.setColorFilter(getBrightnessMatrixColorFilter(brightness));
+    }
+    public static void changeBrightness(Drawable drawable, float brightness) {
+        drawable.setColorFilter(getBrightnessMatrixColorFilter(brightness));
+    }
+
+    private static ColorMatrixColorFilter getBrightnessMatrixColorFilter(float brightness) {
+        ColorMatrix matrix = new ColorMatrix();
+        matrix.set(
+                new float[]{
+                        1, 0, 0, 0, brightness,
+                        0, 1, 0, 0, brightness,
+                        0, 0, 1, 0, brightness,
+                        0, 0, 0, 1, 0
+                });
+        return new ColorMatrixColorFilter(matrix);
+    }
+
+}
